5c07e380d3ed994f112edcac6debd82545f8f7e8
ALEFIYAHMARSHAL <alefiyahmarshal72@gmail.com>
2017-02-10 01:30:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,20 +4,43 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    title : 'Article One | Alefiyah Marshal',
-    heading : ' Article one',
-    date : 'feb 8,2017',
-    content:
-       `<p>
-           This is the content of my new article.This is the content of my new article.This is the content of my new article.This is the content of my new article.
-        </p>
-        <p>
-           This is the content of my new article.This is the content of my new article.This is the content of my new article.This is the content of my new article.
-        </p>
-        <p>
-           This is the content of my new article.This is the content of my new article.This is the content of my new article.This is the content of my new article.
-        </p>`
+var articles = { 
+    'article-one' : {
+        title : 'Article One | Alefiyah Marshal',
+        heading : ' Article one',
+        date : 'feb 8,2017',
+        content:
+           `<p>
+               This is the content of my new article.This is the content of my new article.This is the content of my new article.This is the content of my new article.
+            </p>
+            <p>
+               This is the content of my new article.This is the content of my new article.This is the content of my new article.This is the content of my new article.
+            </p>
+            <p>
+               This is the content of my new article.This is the content of my new article.This is the content of my new article.This is the content of my new article.
+            </p>`
+    
+    },
+    'article-two' : {
+        title : 'Article Two | Alefiyah Marshal',
+        heading : ' Article Two',
+        date : 'feb 9,2017',
+        content:
+           `<p>
+               This is the content of my second article.
+            </p>`
+           
+    },
+    'article-three' : {
+        title : 'Article Three | Alefiyah Marshal',
+        heading : ' Article three',
+        date : 'feb 10,2017',
+        content:
+           `<p>
+               This is the content of my third article.
+            </p>`
+    
+    }
 };
 function createTemplate(data){ 
  var title = data.title;
@@ -63,17 +86,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-    res.send(createTemplate(articleOne));
+app.get('/:articleName',function(req,res){
+    //articleName == article-one
+    //articles[articleName] == {} content object for article-one
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
