4e536a96bcd38cacdf3cbb2cbc0e240484cdf4a7
ALEFIYAHMARSHAL <alefiyahmarshal72@gmail.com>
2017-02-13 00:02:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 //Move the Image
-var img = document.getElementById('alif');
+/*var img = document.getElementById('alif');
 var marginLeft = 0;
 function marginRight () {
    //for(;marginLeft < '50px'; marginLeft = marginLeft + 1){
@@ -17,4 +17,33 @@ function marginRight () {
 img.onclick = function () {
     var interval = setInterval(marginRight,50);
     img.style.marginLeft = '100px';
-};
+};*/
+
+
+var imgObj = null;
+var animate ;
+function init(){
+   imgObj = document.getElementById('alif');
+   imgObj.style.position= 'absolute'; 
+   imgObj.style.top = '240px';
+   imgObj.style.left = '-300px';
+   imgObj.style.visibility='hidden';
+   moveRight();
+} 
+function moveRight(){
+if (parseInt(imgObj.style.left)<=10)
+{
+   imgObj.style.left = parseInt(imgObj.style.left) + 5 + 'px';
+   imgObj.style.visibility='visible';
+   animate = setTimeout(moveRight,20); // call moveRight in 20msec
+   //stopanimate = setTimeout(moveRight,20);
+  }
+else
+  stop();
+  f();
+}
+
+function stop(){
+   clearTimeout(animate);
+}
+window.onload =init;
