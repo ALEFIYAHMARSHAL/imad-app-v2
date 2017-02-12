0897716ca94418acb5444419c44062c46f20d251
ALEFIYAHMARSHAL <alefiyahmarshal72@gmail.com>
2017-02-12 22:59:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
+
+//Move the Image
+var img = document.getElementById('alif');
+img.onclick = function () {
+    img.style.marginleft = '100px';
+};
