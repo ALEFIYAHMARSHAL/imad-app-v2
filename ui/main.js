202db85b7b15524efb51f9d1670a235603e4d4f6
ALEFIYAHMARSHAL <alefiyahmarshal72@gmail.com>
2017-02-12 23:32:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,13 @@ element.innerHTML = 'New value';
 
 //Move the Image
 var img = document.getElementById('alif');
+var marginLeft = 0;
+function marginRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+    }
+    
 img.onclick = function () {
+    var interval = setInterval(marginRight,100);
     img.style.marginLeft = '100px';
 };
