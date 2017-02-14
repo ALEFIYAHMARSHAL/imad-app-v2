3762e34f85556eb17d0d96926e4c238b37f57331
ALEFIYAHMARSHAL <alefiyahmarshal72@gmail.com>
2017-02-15 00:42:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,49 +1,14 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementbyId('counter');
+var counter = 0;
 
-//change the text of the main-text div
-/*var element = document.getElementById('main-text');
-element.innerHTML = 'New value';*/
-
-//Move the Image
-var img = document.getElementById('alif');
-var marginLeft = 0;
-function marginRight () {
-   marginLeft = marginLeft + 1
-   marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-   
-    }
+button.onclick = function(){
+    //Make a request to the counter endpoint
     
-img.onclick = function () {
-    var interval = setInterval(marginRight,50);
-    img.style.marginLeft = '100px';
+    //Capture the response and store it in a variable
+    
+    //Render the variable in a correct span
+    counter = counter + 1;
+    var span = document.getElementbyId('count');
+    span.innerHTML = counter.toString();
 };
-
-
-/*var imgObj = null;
-var animate ;
-function init(){
-   imgObj = document.getElementById('alif');
-   imgObj.style.position= 'absolute'; 
-   imgObj.style.top = '240px';
-   imgObj.style.left = '-300px';
-   imgObj.style.visibility='hidden';
-   moveRight();
-} 
-function moveRight(){
-if (parseInt(imgObj.style.left)<=10)
-{
-   imgObj.style.left = parseInt(imgObj.style.left) + 5 + 'px';
-   imgObj.style.visibility='visible';
-   animate = setTimeout(moveRight,20); // call moveRight in 20msec
-   //stopanimate = setTimeout(moveRight,20);
-  }
-else
-  stop();
-  f();
-}
-
-function stop(){
-   clearTimeout(animate);
-}
-window.onload =init;*/
