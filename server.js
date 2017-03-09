51ce1e3869908644644f7a4637685213ea8205b5
ALEFIYAHMARSHAL <alefiyahmarshal72@gmail.com>
2017-03-10 02:57:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,14 +98,13 @@ var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
 //make a select request
 //return a response with the results
-pool.query('select * from test',function(err,result){
+pool.Query('SELECT * FROM test',function(err, result){
 if(err){
-    res.status(500).send(err,toString());
+    res.status(500).send(err.toString());
 }
 else{
-    res.send(JSON.stringlfy(result));
-}
-    
+    res.send(JSON.stringify(result));
+    }
 });
 });
 var counter = 0;
