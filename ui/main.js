ad95adcf0b2d1e7d2bd69e963c3898472943a488
ALEFIYAHMARSHAL <alefiyahmarshal72@gmail.com>
2017-03-13 03:29:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,9 +30,35 @@ button.onclick = function(){
    var name = nameInput.value;
    var submit = document.getElementById('submit_btn');
    submit.onclick = function() {
+       //Create a request objects
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){ //its check the request of current state object
+        if(request.onreadystatechange === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status === 200){ //this means the request has successfully completed
+    //capture a list of name and render it as a list
+       var names = ['name1', 'name2' ,'name3', 'name4'];
+       //to convrt to html string
+       var list = '';
+       for(i=0; i<names.length; i++) {
+           list += '<li>' + names[i] + '</li>';
+       }
+           //to insert elemnt in our uordered list
+       var ul = document.getElementById('namelist');
+       ul.innerHTML = list;
+            } 
+        }
+        //Not Done yet ignore it
+    };
+    //Make a request to the counter endpoint
+        request.open('GET','http://alefiyahmarshal.imad.hasura-app.io/submit-name?name ='+ name,true);
+        request.send(null);
       //Make a request to the server and send the name
       //capture a list of name and render it as a list
-       var names = ['name1', 'name2' ,'name3', 'name4'];
+       var names = request.responseText;
+       names = JSON.parse(names); //to covert string back to obj
        //to convrt to html string
        var list = '';
        for(i=0; i<names.length; i++) {
